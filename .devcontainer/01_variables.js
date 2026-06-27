da2167b494fee9aa5f1c8e60ecc587fc167a9c5d
Akash Chougale <akashchougale577@gmail.com>
2026-06-27 04:47:47 +0000

    defines variables

---
+++ .devcontainer/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "akashchougale@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2
+
+acccountEmail = "akash@gmail.com"
+accountPassword = "56789"
+accountCity = "kolhapur"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity ])
